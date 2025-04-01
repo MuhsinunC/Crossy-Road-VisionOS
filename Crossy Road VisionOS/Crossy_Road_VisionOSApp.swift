57c0d767633c2c3d941d56f244ed99b19659c12d
Muhsinun Chowdhury <11369216+MuhsinunC@users.noreply.github.com>
2025-04-01 16:31:23 -0700

    Initial Commit

---
+++ Crossy Road VisionOS/Crossy_Road_VisionOSApp.swift
@@ -0,0 +1,33 @@
+//
+//  Crossy_Road_VisionOSApp.swift
+//  Crossy Road VisionOS
+//
+//  Created by Muhsinun on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Crossy_Road_VisionOSApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ Crossy Road VisionOSTests/Crossy_Road_VisionOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Crossy_Road_VisionOSTests.swift
+//  Crossy Road VisionOSTests
+//
+//  Created by Muhsinun on 4/1/25.
+//
+
+import Testing
+@testable import Crossy_Road_VisionOS
+
+struct Crossy_Road_VisionOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
